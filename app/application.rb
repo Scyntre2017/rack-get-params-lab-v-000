8e12b112cde76612d61752cdc9465c51fd37ebea
Christopher Jones <cjones999983@gmail.com>
2018-07-14 23:47:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.length == 0
-        resp.write "Your cart is empty."
+        resp.write "Your cart is empty"
       end
     else
       resp.write "Path Not Found"
